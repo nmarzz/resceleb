aa07e9f11b46b271f639266eab886ef7605a89ee
Noah <nm@Ruth.lan>
2021-11-12 09:23:00 -0500

    reorder transforms

--- loaders.py
+++ loaders.py
@@ -11,10 +11,10 @@ parsed_config = yaml.load(config, Loader=yaml.FullLoader)
 
 root = parsed_config['root_to_celeba']
 
-def celeba_loader(batch_size: int, mode:str,distributed: bool = False, split_for_testing:str = None) -> tuple([DataLoader, DataLoader]):    
+def celeba_loader(batch_size: int, mode:str,distributed: bool = False, split_for_testing:str = None) -> tuple([DataLoader, DataLoader]):
     if split_for_testing is not None:
-        if split_for_testing in ['train','val','test']: 
-            data_set = CelebA(root, mode=mode,split=split_for_testing, 
+        if split_for_testing in ['train','val','test']:
+            data_set = CelebA(root, mode=mode,split=split_for_testing,
                 transform=transforms.Compose([
                     transforms.ToTensor(),
                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -23,19 +23,19 @@ def celeba_loader(batch_size: int, mode:str,distributed: bool = False, split_for
             return DataLoader(data_set, batch_size=batch_size, shuffle=False)
         else:
             raise ValueError('Specify a valid split for testing')
-   
+
     # Get the train/val sets
     train_transforms = transforms.Compose([
-        transforms.ToTensor(),
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    
-    train_set = CelebA(root, mode=mode,split='train', 
+
+    train_set = CelebA(root, mode=mode,split='train',
             transform=train_transforms
             )
 
-    val_set = CelebA(root, mode=mode,split='val', 
+    val_set = CelebA(root, mode=mode,split='val',
             transform=transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -59,23 +59,23 @@ def celeba_loader(batch_size: int, mode:str,distributed: bool = False, split_for
 
 
 
-def cifar10_loader(batch_size: int, mode:str,distributed: bool = False) -> tuple([DataLoader, DataLoader]):    
+def cifar10_loader(batch_size: int, mode:str,distributed: bool = False) -> tuple([DataLoader, DataLoader]):
     # Get the train/val sets
-    
-    train_set = torchvision.datasets.CIFAR10('data', train=True, 
+
+    train_set = torchvision.datasets.CIFAR10('data', train=True,
             transform=transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                             std=[0.229, 0.224, 0.225])
             ]),download = True)
 
-    val_set = torchvision.datasets.CIFAR10('data', train=False, 
+    val_set = torchvision.datasets.CIFAR10('data', train=False,
             transform=transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                             std=[0.229, 0.224, 0.225])
             ]),download = True)
-        
+
     # For distributed training
     if distributed:
         sampler = DistributedSampler(train_set)
@@ -89,5 +89,4 @@ def cifar10_loader(batch_size: int, mode:str,distributed: bool = False) -> tuple
 
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 
-    return train_loader,val_loader    
-
+    return train_loader,val_loader
